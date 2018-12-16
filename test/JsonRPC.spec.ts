68f51bdac026404c554a4a3d46fb54b4660659b7
Grid Cat <gridcat@protonmail.com>
2018-12-16 11:47:13 +0100

    style(package): prettier ate my semi

--- test/JsonRPC.spec.ts
+++ test/JsonRPC.spec.ts
@@ -1,17 +1,18 @@
-import nock from 'nock'
-import { expect } from 'chai'
-import JsonRpc from '../src/JsonRPC'
+import nock from 'nock';
+import { expect } from 'chai';
+import JsonRpc from '../src/JsonRPC';
 
 nock('http://host1.com:80')
   .get('/')
-  .reply(200, 'ok')
+
+  .reply(200, 'ok');
 
 describe('JsonRpc', () => {
   it('should return promise', () => {
     const jsonRPC = new JsonRpc({
       host: 'host1.com',
-      port: 80
-    })
-    expect(jsonRPC.request('', [])).to.be.instanceof(Promise)
-  })
-})
+      port: 80,
+    });
+    expect(jsonRPC.request('', [])).to.be.instanceof(Promise);
+  });
+});
